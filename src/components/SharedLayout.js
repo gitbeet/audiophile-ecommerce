aa5d4b1e78aae1170ca399f5181c530120f587df
gitbeet <nikig715@gmail.com>
2022-10-28 15:48:30 +0200

    fixed bugs with stacking context and text misalign

--- src/components/SharedLayout.js
+++ src/components/SharedLayout.js
@@ -1,27 +1,26 @@
-import React from 'react'
-import { Outlet, useLocation } from 'react-router-dom'
-import { usePopUp } from '../context/PopUpContext'
-import CheckoutWindow from './CheckoutWindow'
-import CompanySummary from './CompanySummary'
-import Footer from './Footer'
-import MobileMenu from './MobileMenu'
-import Nav from './Nav'
-import SuccessfulOrderWindow from './SuccessfulOrderWindow'
+import React from "react";
+import { Outlet, useLocation } from "react-router-dom";
+import { usePopUp } from "../context/PopUpContext";
+import CheckoutWindow from "./CheckoutWindow";
+import CompanySummary from "./CompanySummary";
+import Footer from "./Footer";
+import MobileMenu from "./MobileMenu";
+import Nav from "./Nav";
+import SuccessfulOrderWindow from "./SuccessfulOrderWindow";
 
 export default function SharedLayout() {
-
-  const {showCheckoutWindow,showSuccessfulOrderWindow} = usePopUp()
-  const location = useLocation()
+  const { showCheckoutWindow, showSuccessfulOrderWindow } = usePopUp();
+  const location = useLocation();
 
   return (
     <>
-        <Nav homePage={location.pathname === '/' ? true : false}/>
-        <MobileMenu/>
-        {showCheckoutWindow && <CheckoutWindow/>}
-        {showSuccessfulOrderWindow && <SuccessfulOrderWindow />}
-        <Outlet />
-        {location.pathname !== '/checkout' && <CompanySummary />}
-        <Footer />
+      <Nav homePage={location.pathname === "/" ? true : false} />
+      <MobileMenu />
+      {showCheckoutWindow && <CheckoutWindow />}
+      {showSuccessfulOrderWindow && <SuccessfulOrderWindow />}
+      <Outlet />
+      {location.pathname !== "/checkout" && <CompanySummary />}
+      <Footer />
     </>
-  )
+  );
 }
